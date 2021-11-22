cb0716d22302c141939386581abde1750a8457c1
AdriánRicardo <adrianos1802@gmail.com>
2021-11-22 11:54:00 -0600

    PrimeraVersión
    Crearción de pantallas y DB

---
+++ ProyectoEstructuraDeDatosFinal/src/General/ProyectoEstructuraDeDatosFinal.java
@@ -0,0 +1,15 @@
+package General;
+
+import Pantallas.FRM_Entrada;
+
+
+public class ProyectoEstructuraDeDatosFinal {
+
+
+    public static void main(String[] args) {
+      FRM_Entrada frm_Entrada = new FRM_Entrada();
+      frm_Entrada.setVisible(true);
+      frm_Entrada.toFront();
+    }
+    
+}
